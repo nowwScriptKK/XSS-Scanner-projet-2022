cf40a369cf786f68c33813401cdca58c0198b51e
nowwScriptKK <nowwScriptKKK@proton.me>
2025-01-14 21:34:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from xss import XssScanner
 from time import sleep
 import argparse
 
-def _run(url, data, proxy, wordlist, use_proxy_list):
+def _run(url, data, proxy, wordlist, use_proxy_list, proxy_list_file):
     if data:
         print('Set send POST')
         methode = 'post'
@@ -16,17 +16,17 @@ def _run(url, data, proxy, wordlist, use_proxy_list):
     except:
         pass
 
-    lunch = XssScanner(url, wordlist, methode, proxy, data, use_proxy_list)
+    lunch = XssScanner(url, wordlist, methode, proxy, data, use_proxy_list, proxy_list_file)
     lunch.run()
 
 def __main():
-    data = False
     parser = argparse.ArgumentParser(prefix_chars='-')
     parser.add_argument('-l', type=str, help='URL type is https://link.com/?data1={{inject}}&?data2={{inject}}\nPayload injected in "inject"')
     parser.add_argument("-d", required=False, help='Use data POST send data1={{inject}}#data2={{inject}}\nPayload injected in "inject"')
-    parser.add_argument("-w", required=False, help='If you use other wordlist(default is : files/wordlist/low.txt)"')
+    parser.add_argument("-w", required=False, help='Specify the wordlist file (default is : files/wordlist/low.txt)')
     parser.add_argument("-p", required=False, help='Use a single proxy (format: http://user:pass@host:port or http://host:port)')
-    parser.add_argument("-s", action='store_true', help='Use a proxy from the file /files/proxylist.txt for each request')
+    parser.add_argument("-s", action='store_true', help='Use a proxy from the specified file for each request')
+    parser.add_argument("-f", required=False, help='Specify the proxy list file (default is : files/proxylist.txt)')
 
     args = parser.parse_args()
 
@@ -42,6 +42,9 @@ def __main():
         args.l = 'http://localhost/xsstest.php?q='
     print('default url :'+args.l)
 
+    if not args.f:
+        args.f = "files/proxylist.txt"
+
     return args
 
 print(" _   _     _       _       _   _                  _ _       _     \n| |_| |__ (_)___  (_)___  | |_| |__   ___    __ _(_| |_ ___| |__  \n| __| '_ \| / __| | / __| | __| '_ \ / _ \  / _` | | __/ __| '_ \ \n| |_| | | | \__ \ | \__ \ | |_| | | |  __/ | (_| | | || (__| | | |\n \__|_| |_|_|___/ |_|___/  \__|_| |_|\___|  \__, |_|\__\___|_| |_|\n                                            |___/                 \n")
@@ -49,4 +52,4 @@ if __name__ == '__main__':
     print('With run use\npython3 ./main.py -l https://link.com/?data1={{inject}}&?data2={{inject}}\n\nIf you use post data\npython3 ./main.py -l https://link.com/ -d data1={{inject}}#data2={{inject}}&data3=text \n\n')
 
     parse = __main()
-    _run(parse.l, parse.d, parse.p, parse.w, parse.s)
+    _run(parse.l, parse.d, parse.p, parse.w, parse.s, parse.f)
